ac6bf67f65ac8e021e1b1d6dee68cefea8928192
Joseph J.C. Tang <jinntrance@gmail.com>
2014-12-04 11:11:50 +0800

    refactoring the code

--- syn_gitcafe.sh
+++ syn_gitcafe.sh
@@ -1,6 +1,9 @@
 #!/bin/bash 
 git pull 
 git push
+git checkout gitcafe-pages
+git merge gh-pages
 #git remote add  gc git@gitcafe.com:jinntrance/blog.git 
-git push gc gh-pages:gitcafe-pages
+git push gc gitcafe-pages
+git checkout gh-pages
 
