8e054440a99516e6c3d40c3114a3f70d302d2e15
Joseph J.C. Tang <jinntrance@gmail.com>
2015-05-01 01:09:55 +0800

    add contents

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,13 +3,14 @@
   var headers = $('.entry-content :header').map(function (idx, h){
     var a = $('<a href="#">');
     a.attr('data-id',h.id);
-    a.attr('class', h.nodeName);
+    a.attr('href', '#' + h.id);
+    a.attr('class', h.nodeName.toLowerCase());
     a.html(h.textContent)
     return $('<li>').append(a)[0].outerHTML;
   }).toArray().join("");
-  $('#main').append($('<div id="headers">').html(headers))
+  $('body').append($('<div id="headers">').html($('<ul>').append(headers)));
   $('#headers a').click(function (){
     console.info("scrollTo #" + $(this).attr('data-id'));
-    $('#'+$(this).attr('data-id')).scrollTo(600);
+    //$('#'+$(this).attr('data-id')).scrollTo(600);
   })
  })
