b8b0dcc395a5c0cd9422abbd770edbf6a121430c
Joseph J.C. Tang <jinntrance@gmail.com>
2015-04-30 23:54:41 +0800

    add scroll

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,15 @@
  $(function(){
   $('.entry-content a').attr('target','_blank'); 
+  var headers = $('.entry-content :header').map(function (idx, h){
+    var a = $('<a href="#">');
+    a.attr('data-id',h.id);
+    a.attr('class', h.nodeName);
+    a.html(h.textContent)
+    return $('<li>').append(a)[0].outerHTML;
+  }).toArray().join("");
+  $('#main').append($('<div id="headers">').html(headers))
+  $('#headers a').click(function (){
+    console.info("scrollTo #" + $(this).attr('data-id'));
+    $('#'+$(this).attr('data-id')).scrollTo(600);
+  })
  })
