d92f664c9dac120604d167bd8c6589cce2f68b45
Joseph J.C. Tang <jinntrance@gmail.com>
2015-05-19 00:06:56 +0800

    accept gh-pages when merged to gc-pages

--- syn_gitcafe.sh
+++ syn_gitcafe.sh
@@ -3,7 +3,7 @@ git pull
 git push
 git checkout gitcafe-pages
 export GIT_MERGE_AUTOEDIT=no
-git merge gh-pages
+git merge -X theirs gh-pages
 #git remote add  gc git@gitcafe.com:jinntrance/blog.git 
 git push gc gitcafe-pages
 git checkout gh-pages
