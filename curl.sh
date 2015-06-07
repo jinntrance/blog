36625865cf28006a79e7cd6a283077978173316a
Joseph J.C. Tang <jinntrance@gmail.com>
2015-06-07 19:39:55 +0800

    add script for baidu urls submision

---
+++ curl.sh
@@ -0,0 +1,2 @@
+find  _site | sed 's/_site/http:\/\/blog.josephjctang.com/g' | grep "ml$" >> url.txt
+curl -H 'Content-Type:text/plain' --data-binary @url.txt "http://data.zz.baidu.com/urls?site=blog.josephjctang.com&token=q75fN10vbLmfLVFg"
