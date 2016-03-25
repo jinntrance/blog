08a9ef5aa5fdde7460ad3b98f5be1f2392e8b2cc
Joseph J.C. Tang <jinntrance@gmail.com>
2016-03-25 12:58:01 +0800

    migrate to coding.net

--- syn_gitcafe.sh
+++ syn_gitcafe.sh
@@ -2,10 +2,10 @@
 git checkout gh-pages
 git pull 
 git push
-git checkout gitcafe-pages
+git checkout coding-pages
 export GIT_MERGE_AUTOEDIT=no
 git merge -X theirs gh-pages
-#git remote add  gc git@gitcafe.com:jinntrance/blog.git 
-git push gc gitcafe-pages
+#git remote add  coding git@git.coding.net:jinntrance/blog.git 
+git push coding coding-pages
 git checkout gh-pages
 
