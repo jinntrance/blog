1d4bdee7d7e11c952e87ff94148ffa2fae071e00
Joseph Jin-Chuan TANG <jinntrance@gmail.com>
2020-05-04 00:37:51 +0800

    Update syn_gitcafe.sh

--- syn_gitcafe.sh
+++ syn_gitcafe.sh
@@ -1,13 +1,12 @@
-#!/bin/bash 
+#!/bin/bash
 git checkout gh-pages
-git pull 
+git pull
 git push
 git checkout coding-pages
 export GIT_MERGE_AUTOEDIT=no
 git merge -X theirs gh-pages
-#git remote add  coding git@git.coding.net:jinntrance/blog.git 
+#git remote add  coding git@e.coding.net:jinntrance/blog.git
 #git fetch coding
 #git branch coding-pages coding/coding-pages
 git push coding coding-pages
 git checkout gh-pages
-
